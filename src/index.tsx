d24a3920fdb30962b5364d604d5b8baa0a0388d7
Max-Dov <maksimdovydenko@protonmail.com>
2020-03-27 20:23:34 +0300

    init project structure

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Surfaces from './Surfaces';
+
+ReactDOM.render(<Surfaces />, document.getElementById('root'));
